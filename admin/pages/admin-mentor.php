6b08ffc89f3e0db6cb86ed9fee8aa637cf0d0a96
daaffalbari <daffa.10120212@mahasiswa.unikom.ac.id>
2022-08-08 11:01:16 +0700

    add admin mentor

--- admin/pages/admin-mentor.php
+++ admin/pages/admin-mentor.php
@@ -28,9 +28,76 @@ include_once("layout.php");
 
     <!-- Main content -->
     <section class="content">
-      <a class="btn btn-app">
+    <?php 
+        if(isset($_GET["success"]))
+        {
+            $success = $_GET["success"];
+            if($success == 1)
+              showSuccess("Data berhasil disimpan.");
+            else if($success == 2)
+              showSuccess("Data berhasil diubah.");
+            else if($success == 3)
+              showSuccess("Data berhasil dihapus.");
+        }
+      ?>
+      <button type="button" class="btn btn-app" data-toggle="modal" data-target="#modal-lg">
         <i class="fas fa-plus"></i> Tambah
-      </a>
+      </button>
+
+      <!-- Modal tambah mentor -->
+      <div class="modal fade" id="modal-lg">
+        <div class="modal-dialog modal-lg">
+          <form action="" method="post">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h4 class="modal-title">Tambah Data Mentor</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="form-group">
+                  <label for="id_mentor">ID Mentor</label>
+                  <input type="text" class="form-control" id="id_mentor" name="id_mentor" value="<?php echo kodeOtomatisMentor()?>" readonly>
+                </div>
+                <div class="form-group">
+                  <label for="nama_mentor">Nama Mentor</label>
+                  <input type="text" class="form-control" id="nama_mentor" name="nama_mentor" placeholder="Masukan Nama Mentor" autocomplete="off" required>
+                </div>
+                <div class="form-group">
+                  <label for="jk">Jenis Kelamin</label>
+                  <select class="form-control" id="jk" name="jk" required>
+                    <option value="">Pilih Jenis Kelamin</option>
+                    <option value="Laki-laki">L</option>
+                    <option value="Perempuan">P</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <label for="alamat">Alamat</label>
+                  <textarea class="form-control" id="alamat" name="alamat" placeholder="Masukan Alamat" required></textarea>  
+                </div>
+                <div class="form-group">
+                  <label for="no_hp">No. HP</label>
+                  <input type="text" class="form-control" id="no_hp" name="no_hp" placeholder="Masukan No. HP" autocomplete="off" required>
+                </div>
+                <div class="form-group">
+                  <label for="username">Username</label>
+                  <input type="text" class="form-control" id="username" name="username" placeholder="Masukan Username" autocomplete="off" required>
+                </div>
+                <div class="form-group">
+                  <label for="password">Password</label>
+                  <input type="password" class="form-control" id="password" name="password" placeholder="Masukan Password" autocomplete="off" required>
+                </div>  
+              </div>
+              <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                <button type="submit" class="btn btn-primary" name="btnSimpan">Simpan</button>
+              </div>              
+            </div>
+          </form>
+        </div>
+      </div>
+
       <div class="container-fluid">
         <div class="row">
           <div class="col-12">
